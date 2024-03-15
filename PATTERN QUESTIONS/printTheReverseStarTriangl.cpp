be15300cd2d07634d5973b29b6c4a985bf6a6f48
abhishek kumar <abhishekkuamrr841207@gmail.com>
2024-03-15 21:26:50 +0530

    add new file

--- PATTERN QUESTIONS/printTheReverseStarTriangl.cpp
+++ PATTERN QUESTIONS/printTheReverseStarTriangl.cpp
@@ -3,8 +3,8 @@ using namespace std;
 int main()
 {
     int i,j;
-    for(i=4;i<=1;i++){//outer loop
-        for(j=1;j>=i;j++){//inner loop
+    for(i=3;i>=1;i--){//outer loop
+        for(j=i;j>=0;j--){//inner loop
             cout<<"*";
         }
         cout<<endl;
