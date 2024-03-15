ef8f1241afcb0f3409be8db49ee146eb8dd6b1e3
abhishek kumar <abhishekkuamrr841207@gmail.com>
2024-03-15 17:48:18 +0530

    add new file

---
+++ PRACTICE OF  C++/printSolidRectangle.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int rows,columns,i,j;
+    cout<<"Enter the number of rows=";
+    cin>>rows;
+    cout<<"Enter the number of columns=";
+    cin>>columns;
+
+    for(i=1;i<=rows;i++){
+        for(j=1;j<=columns;j++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
