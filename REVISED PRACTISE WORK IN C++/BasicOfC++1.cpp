bb40a64b23467402cb0f57116c5b610eb2405cec
abhishek kumar <abhishekkuamrr841207@gmail.com>
2024-03-20 21:31:37 +0530

    add new file

--- REVISED PRACTISE WORK IN C++/BasicOfC++1.cpp
+++ REVISED PRACTISE WORK IN C++/BasicOfC++1.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 int main()
 {
-    int E,M,H,S,C,total,percentage;
+    int E,M,H,S,C,total,percentage,average;
     cout<<"Enter the number of Subject English:";
     cin>>E;
     cout<<"Enter the number of subject Math:";
@@ -17,6 +17,21 @@ int main()
     cout<<"Total Marks="<<total<<endl;
     percentage=(total*100)/500;
     cout<<"percentage Marks="<<percentage<<endl;
+    average=total/5;
+    cout<<"average="<<average<<endl;
+    //passing marks by every students
+    if(percentage>=65)
+    cout<<"first divison";
+     else if(percentage>=45 && percentage<=60)
+    cout<<"Second divison";
+    else if(percentage>=35 && percentage<=45)
+    cout<<"Third divison";
+    else
+    cout<<"sorry you are fail";
+
+    
+
+
 
     return 0;
 }
