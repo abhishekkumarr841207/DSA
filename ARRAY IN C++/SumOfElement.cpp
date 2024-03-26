94c0840c74c3ed8dfc01c39b933964464bb4372f
abhishek kumar <abhishekkuamrr841207@gmail.com>
2024-03-26 14:34:30 +0530

    add new file

--- ARRAY IN C++/SumOfElement.cpp
+++ ARRAY IN C++/SumOfElement.cpp
@@ -2,9 +2,9 @@
 using namespace std;
 int main ()
 {
-    int i, array[5]={5,3,3,5,5};
+    int i, array[6]={5,3,3,5,5,7};
     int sum=0;
-    for (i=0;i<5;i++){
+    for (i=0;i<6;i++){
         sum+=array[i];
     }
     cout<<sum<<endl;
